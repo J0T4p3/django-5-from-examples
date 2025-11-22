6c098dbfdea38cd8005d0aaf0ff474658567977e
reversejp <jpslima@timbrasil.com.br>
2025-11-22 10:21:03 -0300

    Added pagination and pagination error handlers to posts list view; Changed post details view to utilize SEO friendly links enforcing daily slug uniqueness for post details page

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -18,11 +19,11 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     # Slug is a alphanumeric with hyphen text. Useful for SEO
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='blog_posts'
+        related_name='blog_posts',
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -38,7 +39,6 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-    # A subclass carrying metadata about it's parent class
     class Meta:
         # Results will be returned in reverse cronological publish order by default
         ordering = ['-publish']
@@ -46,6 +46,13 @@ class Post(models.Model):
             models.Index(fields=['publish'])
         ]
 
+    # A unified way to resolve the url for Post data
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month,
+                  self.publish.day, self.slug]
+        )
+
     def __str__(self):
-        return f"{self.title} by {self.author.username} in {self.publish}"
         return f"{self.title} by {self.author.username} in {self.publish}"
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,27 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Paginator with 3 items pe page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If not found, returns the last page
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # If page is another value type than an integer
+        posts = paginator.page(1)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__day=day, publish__month=month, publish__year=year)
     return render(request, 'blog/post/detail.html', {'post': post})
